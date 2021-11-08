9fe93a8c0064f38b7d37be40eed01bd1f2ad01ce
Peter Hutchison <Peterhutch03@gmail.com>
2021-11-08 10:23:29 -0600

    Initial Commit

---
+++ Dicey/DiceyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceyApp.swift
+//  Dicey
+//
+//  Created by Student on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
